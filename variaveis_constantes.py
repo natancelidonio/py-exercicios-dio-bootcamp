dd594923369c37200deb6dfd33d35875f5232a10
Natan Operação <natan.oper@telemedicina.local>
2024-06-08 18:35:15 -0300

    Variáveis e constantes; Conversão de tipos; Funções de entrada e saída

---
+++ variaveis_constantes.py
@@ -0,0 +1,16 @@
+nome = "Natan";
+idade = 31;
+
+nome = "João";
+
+print(nome, idade);
+
+limite_saque_diario = 1000;
+
+print(limite_saque_diario);
+
+BRAZILIAN_STATES = ["SP", "SC", "RJ", "MG",];
+
+BRAZILIAN_STATES = 10;
+
+print(BRAZILIAN_STATES);
